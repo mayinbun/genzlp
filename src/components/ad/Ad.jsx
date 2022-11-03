db09fdef1b36c8f3beac7272fdeaa53d5aefc21c
Bunyamin Yildirim <bunyamin.yildirim@adevinta.com>
2022-11-03 16:12:21 +0100

    style fixes

--- src/components/ad/Ad.jsx
+++ src/components/ad/Ad.jsx
@@ -2,28 +2,27 @@ import { Avatar, Container, ImageList, ImageListItem, Typography } from '@mui/ma
 
 function Ad({ item }) {
   return (
-    <>
-      <Container disableGutters sx={{ display: 'flex', alignItems: 'center', paddingTop: '16px' }}>
+    <Container disableGutters sx={{background: '#fff', marginBottom: '16px'}}>
+      <Container sx={{ display: 'flex', alignItems: 'center', paddingTop: '16px' }}>
         <Avatar sx={{ width: 24, height: 24 }} alt={item.seller}></Avatar>
         <Typography sx={{ marginLeft: '8px' }}>{item.seller}</Typography>
       </Container>
 
       <ImageList key={item.id} sx={{
         gridAutoFlow: 'column',
-        gridAutoColumns: 'minmax(160px, 1fr)'
       }}>
         {(item.mediaUrls).map((media) => (
-          <ImageListItem sx={{ width: 200, height: 235 }} key={media}>
-            <img src={media} alt={item.title} loading="lazy" />
+          <ImageListItem sx={{ width: 200, height: 235, paddingLeft: '16px' }} key={media}>
+            <img style={{borderRadius: '7%'}} src={media} alt={item.title} loading="lazy" />
           </ImageListItem>
         ))}
       </ImageList>
 
-      <Container disableGutters>
-        <Typography>{item.title}</Typography>
-        <Typography>{item.price}</Typography>
+      <Container sx={{display: 'flex'}}>
+        <Typography sx={{maxWidth:'75%'}}>{item.title}</Typography>
+        <Typography fontWeight={700}>{item.price}</Typography>
       </Container>
-    </>
+    </Container>
   );
 }
 
