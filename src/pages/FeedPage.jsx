91068304c6a32915171177af29d1a804bee61da5
Bunyamin Yildirim <bunyamin.yildirim@adevinta.com>
2022-11-03 17:41:23 +0100

    add b2cads section

--- src/pages/FeedPage.jsx
+++ src/pages/FeedPage.jsx
@@ -1,11 +1,18 @@
 import { Container } from '@mui/material';
 import { Ad } from '../components/ad/Ad';
+import { B2cads } from '../components/b2cads/B2cads';
 import data from '../feedMockData.json';
 
 function FeedPage() {
+  const c2cAds = data.items.filter((item) => item.type === 'C2C');
+  const b2cAds = data.items
+    .filter((item) => item.type === 'B2C')
+
   return (
     <Container sx={{ background: '#F7F7F6' }}>
-      {data.items.map((item) => <Ad key={item.id} item={item} />)}
+      {c2cAds.map((item, i) => {
+        return i === 2 ? <B2cads key={i} items={b2cAds} /> : <Ad key={item.id} item={item} />
+      })}
     </Container>
   );
 }
