7a590d9b9a2a280efb9bd735170a18988b680859
Bogdan Deren <None>
2022-11-03 16:00:57 +0100

    cleanup

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,11 +1,8 @@
-import AdbIcon from '@mui/icons-material/Adb';
-
-import { AppBar, Avatar, Box, Container, IconButton, Toolbar, Tooltip, Typography } from '@mui/material';
+import { AppBar } from '@mui/material';
 import * as React from 'react';
 
 
 function Nav() {
-  const handleOpenUserMenu = (event) => {};
 
   return (
     <AppBar position="sticky">
