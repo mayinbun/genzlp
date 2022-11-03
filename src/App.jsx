a43d69996a9c5348d0ab0ecee89a03ab59333f68
Bogdan Deren <None>
2022-11-03 12:16:51 +0100

    categories component

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Categories from './components/categories/Categories';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <Categories />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/categories/Categories.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Categories = () => {
+    return (
+        <ul className="categories">
+            <li>Audio, TV en Foto</li>
+            <li>Auto's</li>
+            <li>Boeken</li>
+            <li>Computers en Software</li>
+            <li>Fietsen</li>
+            <li>Dames Kleding</li>
+            <li>Heren Kleding</li>
+            <li>Muziek en Instrumenten</li>
+            <li>Mobiele telefoons</li>
+            <li>Tickets</li>
+        </ul>
+    )
+}
+
+export default Categories;
