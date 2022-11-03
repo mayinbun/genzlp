62a37b9d40a81a226d287e47ab34c55e226646f3
Bogdan Deren <None>
2022-11-03 15:53:25 +0100

    header image

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -8,38 +8,8 @@ function Nav() {
   const handleOpenUserMenu = (event) => {};
 
   return (
-    <AppBar position="static">
-      <Container maxWidth="xl">
-        <Toolbar disableGutters>
-          <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
-          <Typography
-            variant="h6"
-            noWrap
-            component="a"
-            href="/"
-            sx={{
-              mr: 2,
-              display: { xs: 'flex', md: 'flex' },
-              fontFamily: 'monospace',
-              fontWeight: 600,
-              color: 'inherit',
-              textDecoration: 'none',
-            }}
-          >
-            Marktplaats
-          </Typography>
-
-          <Box sx={{ flexGrow: 1 }}></Box>
-
-          <Box sx={{ flexGrow: 0, alignSelf: 'center' }}>
-            <Tooltip title="Open settings">
-              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
-              </IconButton>
-            </Tooltip>
-          </Box>
-        </Toolbar>
-      </Container>
+    <AppBar position="sticky">
+      <img className='headerImage' src="assets/Header.png" />
     </AppBar>
   );
 }
