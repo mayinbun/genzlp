75b0b8bdda442a91cef32d43a321090774f41958
Bunyamin Yildirim <bunyamin.yildirim@adevinta.com>
2022-11-03 16:27:00 +0100

    fix price

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 import React, { useState } from 'react';
+import './App.css';
 import Categories from './components/categories/Categories';
 import { Nav } from './components/nav/Nav';
-import './App.css';
 import { FeedPage } from './pages/FeedPage';
 
 const App = () => {
--- src/components/ad/Ad.jsx
+++ src/components/ad/Ad.jsx
@@ -2,7 +2,7 @@ import { Avatar, Container, ImageList, ImageListItem, Typography } from '@mui/ma
 
 function Ad({ item }) {
   return (
-    <Container disableGutters sx={{background: '#fff', marginBottom: '16px'}}>
+    <Container disableGutters sx={{ background: '#fff', marginBottom: '16px' }}>
       <Container sx={{ display: 'flex', alignItems: 'center', paddingTop: '16px' }}>
         <Avatar sx={{ width: 24, height: 24 }} alt={item.seller}></Avatar>
         <Typography sx={{ marginLeft: '8px' }}>{item.seller}</Typography>
@@ -13,13 +13,19 @@ function Ad({ item }) {
       }}>
         {(item.mediaUrls).map((media) => (
           <ImageListItem sx={{ width: 200, height: 235, paddingLeft: '16px' }} key={media}>
-            <img style={{borderRadius: '7%'}} src={media} alt={item.title} loading="lazy" />
+            <img style={{ borderRadius: '7%' }} src={media} alt={item.title} loading="lazy" />
           </ImageListItem>
         ))}
       </ImageList>
 
-      <Container sx={{display: 'flex'}}>
-        <Typography sx={{maxWidth:'75%'}}>{item.title}</Typography>
+      <Container sx={{ display: 'flex', justifyContent: 'space-between' }}>
+        <Typography sx={{
+          maxWidth: '80%',
+          textOverflow: 'ellipsis',
+          overflow: 'hidden',
+          whiteSpace: 'nowrap',
+          textAlign: 'left'
+        }}>{item.title}</Typography>
         <Typography fontWeight={700}>{item.price}</Typography>
       </Container>
     </Container>
